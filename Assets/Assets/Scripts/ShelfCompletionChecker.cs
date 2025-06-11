1578926f1fc578ccb14aae9d7182656eb93ecb5d
VDZM <vadim.makarevich@gmail.com>
2025-06-11 13:03:01 +0300

    Simplify shelf completion helper

--- Assets/Assets/Scripts/ShelfCompletionChecker.cs
+++ Assets/Assets/Scripts/ShelfCompletionChecker.cs
@@ -1,14 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 
 public class ShelfCompletionChecker : MonoBehaviour
 {
-    [SerializeField] private GameObject levelCompletePanel;
-    [SerializeField] private Button nextLevelButton;
-    [SerializeField] private Button exitButton;
-
     public bool AreAllShelvesEmpty(List<CatSortMode.Shelf> shelves)
     {
         foreach (var shelf in shelves)
@@ -31,58 +25,7 @@ public class ShelfCompletionChecker : MonoBehaviour
         if (GameModeManager.Instance != null)
         {
             GameModeManager.Instance.SetGameActive(false);
-        }
-
-        ShowLevelCompletePanel(shelves.Count * 4);
-    }
-
-    private void ShowLevelCompletePanel(int rawScore)
-    {
-        int current = PlayerPrefs.GetInt("CatSortLevel", 0);
-        PlayerPrefs.SetInt("CatSortLevel", current + 1);
-        PlayerPrefs.Save();
-
-        Time.timeScale = 0f;
-        if (levelCompletePanel != null)
-        {
-            levelCompletePanel.SetActive(true);
-        }
-
-        if (nextLevelButton != null)
-        {
-            nextLevelButton.onClick.RemoveAllListeners();
-            nextLevelButton.onClick.AddListener(() =>
-            {
-                Time.timeScale = 1f;
-                if (levelCompletePanel != null)
-                {
-                    levelCompletePanel.SetActive(false);
-                }
-                if (GameModeManager.Instance != null && GameModeManager.Instance.catSortMode != null)
-                {
-                    GameModeManager.Instance.catSortMode.ResetLevel();
-                    GameModeManager.Instance.catSortMode.GenerateLevel();
-                }
-            });
-        }
-
-        if (exitButton != null)
-        {
-            exitButton.onClick.RemoveAllListeners();
-            exitButton.onClick.AddListener(() =>
-            {
-                Time.timeScale = 1f;
-                if (levelCompletePanel != null)
-                {
-                    levelCompletePanel.SetActive(false);
-                }
-                SceneManager.LoadScene("ModeSelectScene");
-            });
-        }
-
-        if (GameModeManager.Instance != null)
-        {
-            GameModeManager.Instance.ShowLevelCompletePanel(rawScore, 0, 0);
+            GameModeManager.Instance.ShowLevelCompletePanel(shelves.Count * 4, 0, 0);
         }
     }
 }
